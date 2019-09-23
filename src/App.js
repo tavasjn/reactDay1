f095d76a3815897861252fd6ed7cc92619caa3cb
Tavas Neuwirth <tavasjn_24@hotmail.com>
2019-09-23 13:49:42 -0600

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friends: [],
+      picture: '',
+      name: '',
+    }
+  }
+
+  updatePicture(value){
+    this.setState({
+      picture: value
+    })
+  }
+
+  updateName(value){
+    this.setState({
+      name: value
+    })
+  }
+
+  addFriend(){
+    const {friends, picture, name} = this.state;
+
+    let newFriends = friends.slice();
+    newFriends.push({picture, name});
+
+    this.setState({
+      friends: newFriends,
+      picture: '',
+      name: '',
+    })
+  }
+
+
+
+  render(){
+    // console.log(this.state.picture)
+    // console.log(this.state.name)
+    // console.log(this.state.friends)
+    const friends = this.state.friends.map((element, index) => {
+      return(
+        <div key={index}>
+          <img width="100px" src={element.picture} />
+          <span>{element.name}</span>
+        </div>
+      )
+    })
+
+    return (
+      <div className="App">
+        <label>Picture:</label>
+        <input onChange={(event) => this.updatePicture(event.target.value)} value={this.state.picture}/>
+        <label>Name:</label>
+        <input onChange={(event) => this.updateName(event.target.value)} value={this.state.name}/>
+        <button onClick={() => this.addFriend()}>Add Friend</button>
+        {friends}
+      </div>
+    );
+  }
 }
 
 export default App;
